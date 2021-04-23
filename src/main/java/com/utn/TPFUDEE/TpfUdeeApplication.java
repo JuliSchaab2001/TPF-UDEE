6fd4b3ae1df984a427679fb1f9cb6549b4f0cfc4
unknown <jupobatero1@gmail.com>
2021-04-23 11:54:12 -0300

    Inicializo el proyecto con Spring Initializr

---
+++ src/main/java/com/utn/TPFUDEE/TpfUdeeApplication.java
@@ -0,0 +1,13 @@
+package com.utn.TPFUDEE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpfUdeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpfUdeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utn/TPFUDEE/TpfUdeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.TPFUDEE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpfUdeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
