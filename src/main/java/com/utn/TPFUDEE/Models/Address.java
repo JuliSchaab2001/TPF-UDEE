43f53d3a0f57b8b28a943b7cc233be82d0ff3e0d
unknown <jupobatero1@gmail.com>
2021-04-23 21:55:51 -0300

    Agrego la clase Address DE VERDAD

--- src/main/java/com/utn/TPFUDEE/Models/Address.java
+++ src/main/java/com/utn/TPFUDEE/Models/Address.java
@@ -1,4 +1,9 @@
 package com.utn.TPFUDEE.Models;
 
 public class Address {
+    private Measurement measurement;
+    private Tariff tariff;
+    private String street;
+    private Integer number;
+
 }
