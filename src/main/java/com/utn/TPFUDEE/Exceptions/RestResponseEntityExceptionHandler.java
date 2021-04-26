26995c9b436bc044cc4617c4fda5a113e7be13e8
unknown <jupobatero1@gmail.com>
2021-04-26 20:41:31 -0300

    Agrego manejo de excepciones

---
+++ src/main/java/com/utn/TPFUDEE/Exceptions/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.utn.TPFUDEE.Exceptions;
+
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({ConstraintViolationException.class})
+    public ResponseEntity<Object> handleConstraintViolation(ConstraintViolationException ex, WebRequest request) {
+
+        List<String> errors = new ArrayList<>();
+
+        for (ConstraintViolation violation : ex.getConstraintViolations()) {
+            errors.add(violation.getRootBeanClass().getName() + " " + violation.getMessage());
+        }
+        ApiError apiErrors = new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), errors);
+
+        return new ResponseEntity<Object>(apiErrors, new HttpHeaders(), apiErrors.getHttpStatus());
+    }
+
+}
